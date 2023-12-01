ed17547808993a8667d63529ac1dbc1003b16e21
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-12-01 22:06:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+/* Your CSS Code here. */
+.accio{
+	height:150px;
+	width:150px;
+	background-color:black;
+}
+.hello-accio{
+	color:white;
+	margin-left:32px;
+	padding:10px;
+}
